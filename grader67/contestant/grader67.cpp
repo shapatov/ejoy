4747ca6f4ec605b39eff464cc882d26d8810d067
usarana <policaicho@gmail.com>
2025-11-02 15:25:31 +0200

    major overhaul

--- grader67/contestant/grader67.cpp
+++
@@ -1,8 +0,0 @@
-#include "grader67.h"
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int n; cin >> n;
-    vector<int> ans = solve(n);
-    cout << 1 << endl;
-}
--- grader67/system/grader67.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <vector>
-using namespace std;
-vector<int> solve(int);
